460629d2965aeac52e3cd19891eb923f222295ec
Dylan White <dylan.whitej@gmail.com>
2021-04-01 13:18:53 -0700

    aws set up

---
+++ sql/setup.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS uploads;
+
+
+CREATE TABLE uploads (
+        id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        url TEXT NOT NULL,
+        time_stamp INT NOT NULL,
+        name TEXT NOT NULL,
+        type TEXT NOT NULL
+)
