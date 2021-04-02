7910e06289c4ac49242384214a1de88921d28c4c
Dylan White <dylan.whitej@gmail.com>
2021-04-01 17:56:22 -0700

    delete route working

--- sql/setup.sql
+++ sql/setup.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS uploads;
 CREATE TABLE uploads (
         id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
         url TEXT NOT NULL,
-        time_stamp INT NOT NULL,
+        time_stamp BIGINT NOT NULL,
         name TEXT NOT NULL,
         type TEXT NOT NULL
 )
