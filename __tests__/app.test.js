662b14fc7f4655812f4eb0b32509bb46e5d5b29d
Dylan White <dylan.whitej@gmail.com>
2021-04-06 11:48:55 -0700

    adds test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,62 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+jest.mock('../lib/utils/amazon.js', () => {
+    return {
+        single: () => (req, res, next) => {
+            req.file = {
+                location:
+                    'https://my-first-bucket-cool.s3.us-west-2.amazonaws.com/94445dc1-d675-4ace-bb0d-83d7242ad352-image',
+                key: '55555',
+                contentType: 'image',
+            };
+            req.body = {
+                name: 'myFile',
+            };
+            next();
+        },
+    };
+});
+
 describe('. routes', () => {
-  beforeEach(() => {
-    return setup(pool);
-  });
+    beforeEach(() => {
+        return setup(pool);
+    });
+    it('should add an image to the database', async () => {
+        const data = await request(app)
+            .post('/api/v1/upload/file')
+            .send({ name: 'myFile', image: 'hello.jpg' });
+        const expectation = {
+            id: '1',
+            name: 'myFile',
+            timeStamp: '55555',
+            url:
+                'https://my-first-bucket-cool.s3.us-west-2.amazonaws.com/94445dc1-d675-4ace-bb0d-83d7242ad352-image',
+        };
+
+        expect(data.body).toEqual(expectation);
+    });
+
+    it('should delete an image from the database and get remaining image from database', async() => {
+      await request(app)
+            .post('/api/v1/upload/file')
+            .send({ name: 'myFile', image: 'hello.jpg' });
+      await request(app)
+            .post('/api/v1/upload/file')
+            .send({ name: 'myFile', image: 'hello.jpg' });
+      await request(app)
+            .delete('/api/v1/upload/file/2')
+      const data = await request(app)
+            .get('/api/v1/upload/files')
+
+      const expectation = {
+        id: '1',
+        name: 'myFile',
+        timeStamp: '55555',
+        url:
+            'https://my-first-bucket-cool.s3.us-west-2.amazonaws.com/94445dc1-d675-4ace-bb0d-83d7242ad352-image',
+    };
+
+      expect(data.body[0]).toEqual(expectation)
+    });
 });
